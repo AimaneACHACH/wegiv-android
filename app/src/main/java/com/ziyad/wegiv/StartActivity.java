645101480cc961e9750f42233d312a48f4852884
ZIYAD MABROUK <mabrouk.ziyad@inemail.ine.inpt.ma>
2023-05-16 11:59:47 +0100

    Initial commit

---
+++ app/src/main/java/com/ziyad/wegiv/StartActivity.java
@@ -0,0 +1,42 @@
+package com.ziyad.wegiv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class StartActivity extends AppCompatActivity {
+
+    ImageView app_logo, app_name;
+    TextView app_slogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        app_logo = findViewById(R.id.app_logo);
+        app_name = findViewById(R.id.app_name);
+        app_slogan = findViewById(R.id.app_slogan);
+
+        Animation fade_in = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        Animation fade_out = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+
+        app_logo.setAnimation(fade_in);
+        app_name.setAnimation(fade_in);
+        app_slogan.setAnimation(fade_in);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(StartActivity.this, LogInActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
